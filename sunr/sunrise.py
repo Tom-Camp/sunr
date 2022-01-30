2c34ace43fb6c9b19b8e9c879b96bb578afc4b88
Tom Camp <tom.camp@civicactions.com>
2022-01-30 11:36:47 -0500

    Sunrise test.

--- sunr/sunrise.py
+++ sunr/sunrise.py
@@ -1,7 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple test for NeoPixels on Raspberry Pi
 import time
 
 import board
@@ -12,11 +8,7 @@ delay = 2
 brighness = 0.1
 
 pixels = neopixel.NeoPixel(
-    board.D18,
-    32,
-    brightness=0.1,
-    auto_write=False,
-    pixel_order=neopixel.RGB
+    board.D18, 32, brightness=0.1, auto_write=False, pixel_order=neopixel.RGB
 )
 
 up = [
@@ -28,13 +20,14 @@ up = [
     [0, 7, 8, 15, 16, 23, 24, 31],
 ]
 
+
 def sunrise():
     """
     Start the sun to risin'
     """
-    for l in up:
-        for r in l:
-            pixels[r] = sky_blue
+    for row in up:
+        for p in row:
+            pixels[p] = sky_blue
         pixels.show()
         time.sleep(delay)
 
@@ -42,6 +35,7 @@ def sunrise():
 def stop():
     pixels.fill((0, 0, 0))
     pixels.show()
+
 
 try:
     sunrise()
