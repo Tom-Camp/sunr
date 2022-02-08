04bbd061cd843a64c36301832e13f5ef24dfc38d
Tom Camp <tomcamp@protonmail.ch>
2022-02-07 21:18:33 -0500

    Adding config tests.

--- tests/test_sunr.py
+++ tests/test_sunr.py
@@ -1,5 +1,23 @@
 from sunr import __version__
+import json
+
+
+with open('configuration.json', 'r') as f:
+    config = json.load(f)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_load_config():
+    assert type(config) is dict
+
+
+def test_config_color():
+    assert len(config.get('color')) == 3
+
+
+def test_config_delay():
+    assert config.get('delay') > 0
+    
