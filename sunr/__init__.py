0f18691c3d4d2b9e3f056820da1a757e3ce85b9b
Tom Camp <tomcamp@protonmail.ch>
2022-02-12 06:02:02 -0500

    Adding configuration copying.

--- sunr/__init__.py
+++ sunr/__init__.py
@@ -1,1 +1,10 @@
 __version__ = '0.1.0'
+
+import shutil
+from pathlib import Path
+
+dest = Path('configuration.json')
+
+if not dest.exists():
+    src = Path('configuration.json.example')
+    shutil.copy(src, dest)
